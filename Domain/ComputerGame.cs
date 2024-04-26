6fac683a74faa5a6e1884c98ab7fee948b0af088
Администратор <@SUHROBSAMADOV>
2024-04-26 11:19:33 +0500

    Добавьте файлы проекта.

---
+++ Domain/ComputerGame.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class ComputerGame : BaseEntity
+    {
+        public string Title { get; set; }
+        public int ReleaseYear { get; set; }
+        public decimal Price { get; set; }
+        public string Genre { get; set; }
+        public string Studio { get; set; }
+    }
+}
---
+++ Domain/GameFilter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class GameFilter : BaseEntity
+    {
+        public int MinReleaseYear { get; set; }
+        public int MaxReleaseYear { get; set; }
+        public decimal MinPrice { get; set; }
+        public decimal MaxPrice { get; set; }
+        public string Genre { get; set; }
+        public string Studio { get; set; }
+    }
+}
